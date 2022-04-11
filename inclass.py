69d5641c848ce815c66ba0d80fa59c1abe73067c
Sofia Murillo <55448941+sofiamurillosanchez@users.noreply.github.com>
2022-04-11 16:04:20 -0500

    Create inclass.py

---
+++ inclass.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.title('In-Class Example - CS5394')
+
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+def increment_counter():
+    st.session_state.count += 1
+
+st.button('Click Me!', on_click=increment_counter)
+
+st.write('Count = ', st.session_state.count)
+
+data = pd.read_csv('/Users/sofiamurillosanchez/Downloads/Bike-Sharing-Dataset/hour.csv')
+st.write(data)
+
+hist_values = np.histogram(
+    data['windspeed'], bins=24)[0]
+
+st.bar_chart(hist_values)
