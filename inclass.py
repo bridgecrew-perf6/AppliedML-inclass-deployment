85ab9af62bd22f0c8f7679ba75f0f90984832287
Sofia Murillo <55448941+sofiamurillosanchez@users.noreply.github.com>
2022-04-13 16:24:54 -0500

    Add comment section and update files accordingly

--- inclass.py
+++ inclass.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+# For comment section, referenced below
+from datetime import datetime
+from vega_datasets import data
+from utils import db
+
 
 # Reference: https://github.com/streamlit/example-app-commenting/blob/main/streamlit_app.py
 def space(num_lines=1):
@@ -41,4 +46,29 @@ space(3)
 # Reference: https://github.com/streamlit/example-app-commenting
 st.title("Comment Section 💬")
 
+ st.write("**Comments:**")
 
+    for index, entry in enumerate(comments.itertuples()):
+        st.markdown(COMMENT_TEMPLATE_MD.format(entry.name, entry.date, entry.comment))
+
+        is_last = index == len(comments) - 1
+        is_new = "just_posted" in st.session_state and is_last
+        if is_new:
+            st.success("☝️ Your comment was successfully posted.")
+
+    space(2)
+
+    # Insert comment
+
+    st.write("**Add your own comment:**")
+    form = st.form("comment")
+    name = form.text_input("Name")
+    comment = form.text_area("Comment")
+    submit = form.form_submit_button("Add comment")
+
+    if submit:
+        date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        db.insert(conn, [[name, comment, date]])
+        if "just_posted" not in st.session_state:
+            st.session_state["just_posted"] = True
+        st.experimental_rerun()
---
+++ utils/constants.py
@@ -0,0 +1,6 @@
+SCOPE = "https://www.googleapis.com/auth/spreadsheets"
+SPREADSHEET_ID = "1SQwfmTdIHzMlRbALS1DJHK8bEn9RL5HlKnmD6qstOVM"
+SHEET_NAME = "Comment Section"
+GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
+COMMENT_TEMPLATE_MD = """{} - {}
+> {}"""
